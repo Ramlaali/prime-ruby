353723c325e86bf0fd040e85c00b9806d4cbbb5b
Ramla <ramlaali.abd@gmail.com>
2021-03-04 20:14:59 -0500

    commit tree ec72086e5c827fb5bfb1d2a727af269122594a1f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(numbers)
+    if numbers <= 1
+        return false
+    else 
+        (2..numbers -1).to_a.all? do |integer|
+            numbers % integer != 0
+        end
+        
+    end
+end
